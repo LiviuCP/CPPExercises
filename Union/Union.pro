bb0c903cb4d410ebb49caf25c3435dbde694fefa
Liviu Constantin POPA <liviucst@gmail.com>
2020-09-27 13:19:08 +0300

    Created some unions
    It was required to determine primitive variables sizes without using sizeof and to determine if the computer architecture is big or little endian

---
+++ Union/Union.pro
@@ -0,0 +1,7 @@
+TEMPLATE = app
+CONFIG += console c++11 c++1z
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        main.cpp
---
+++ Union/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+static constexpr size_t c_MaskSize{32};
+
+union Primitives
+{
+    bool boolean;
+    int integer;
+    short shortInt;
+    long longInt;
+    long long longLongInt;
+    float decFloat;
+    double decDouble;
+    long double decLongDouble;
+    char mask[c_MaskSize];
+};
+
+union Endian
+{
+    uint16_t number;
+    byte bytes[2];
+};
+
+int main()
+{
+    Primitives src, dest;
+    Endian endian;
+
+    cout << "Created union. We will test some stuff by using it." << endl << endl;
+    cout << "First, let's retrieve the byte sizes of some primitive variable types" << endl;
+
+    memset(src.mask, 0xFF, c_MaskSize);
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.boolean = src.boolean;
+    cout << "* The size of the boolean is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.integer = src.integer;
+    cout << "* The size of the integer is: " << static_cast<int>(strlen(dest.mask)) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.shortInt = src.shortInt;
+    cout << "* The size of the short integer is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.longInt = src.longInt;
+    cout << "* The size of the long integer is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.longLongInt = src.longLongInt;
+    cout << "* The size of the long long integer is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.decFloat = src.decFloat;
+    cout << "* The size of the float is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.decDouble = src.decDouble;
+    cout << "* The size of the double is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    memset(dest.mask, '\0', c_MaskSize);
+    dest.decLongDouble = src.decLongDouble;
+    cout << "* The size of the long double is: " << strlen(dest.mask) << " byte(s)" << endl;
+
+    cout << endl << "Second, let's determine the byte ordering (little/big endian)" << endl;
+    endian.number = 0xF0F1;
+
+    if (endian.bytes[1] == static_cast<byte>(0xF1))
+    {
+        cout << "* The computer architecture is BIG endian" << endl;
+    }
+    else
+    {
+        cout << "* The computer architecture is LITTLE endian" << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
