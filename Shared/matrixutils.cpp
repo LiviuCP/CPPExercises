86daa4cb2019f51d9ec84721bb2be5241e960edc
Liviu Constantin POPA <liviucst@gmail.com>
2020-12-11 21:09:05 +0200

    Turned the file IO operators for Matrix into templates
    It was required to support reading from and writing to matrixes for any data type that supports the << and >> operators (e.g. double)

--- Shared/matrixutils.cpp
+++ Shared/matrixutils.cpp
@@ -1,66 +1,9 @@
-#include <fstream>
-#include <iostream>
-
 #include "matrixutils.h"
 
-std::ifstream& operator>>(std::ifstream& in, Matrix<int>& data)
+// this (dummy) file is required for preventing any link errors on X-Code (OS-X). Don't fill anything in, don't use it!
+
+// used for suppressing warning message
+int dummyFunctionMatrixUtilsCPP()
 {
-    bool success{false};
-
-    Matrix<int> matrix{};
-
-    int nrOfRows{0};
-    int nrOfColumns{0};
-
-    in >> nrOfRows;
-
-    if (!in.fail() && nrOfRows > 0)
-    {
-        in >> nrOfColumns;
-
-        if (!in.fail() && nrOfColumns > 0)
-        {
-            matrix.resize(nrOfRows, nrOfColumns);
-            success = true;
-
-            for (Matrix<int>::ZIterator it{matrix.zBegin()}; it != matrix.zEnd(); ++it)
-            {
-                in >> *it;
-
-                if (in.fail())
-                {
-                    success = false;
-                    break;
-                }
-            }
-
-            if (success)
-            {
-                data = std::move(matrix);
-            }
-        }
-    }
-
-    // ensure an empty matrix is provided if reading has not been done successfully
-    if (!success)
-    {
-        data.clear();
-    }
-
-    return in;
-}
-
-std::ofstream& operator<<(std::ofstream& out, const Matrix<int>& data)
-{
-    for(int row{0}; row < data.getNrOfRows(); ++row)
-    {
-        for (Matrix<int>::ConstZIterator it{data.constZRowBegin(row)}; it != data.constZRowEnd(row); ++it)
-        {
-            out << *it << " ";
-        }
-
-        out << "\n";
-    }
-
-    return out;
+    return 0;
 }
--- Shared/matrixutils.h
+++ Shared/matrixutils.h
@@ -5,7 +5,75 @@
 
 #include "../External/Matrix/MatrixLib/matrix.h"
 
-std::ifstream& operator>>(std::ifstream& in, Matrix<int>& data);
-std::ofstream& operator<<(std::ofstream& out, const Matrix<int>& data);
+template<typename T>
+std::ifstream& operator>>(std::ifstream& in, Matrix<T>& data);
+
+template<typename T>
+std::ofstream& operator<<(std::ofstream& out, const Matrix<T>& data);
+
+template<typename T>
+std::ifstream& operator>>(std::ifstream& in, Matrix<T>& data)
+{
+    bool success{false};
+
+    Matrix<T> matrix{};
+
+    int nrOfRows{0};
+    int nrOfColumns{0};
+
+    in >> nrOfRows;
+
+    if (!in.fail() && nrOfRows > 0)
+    {
+        in >> nrOfColumns;
+
+        if (!in.fail() && nrOfColumns > 0)
+        {
+            matrix.resize(nrOfRows, nrOfColumns);
+            success = true;
+
+            for (typename Matrix<T>::ZIterator it{matrix.zBegin()}; it != matrix.zEnd(); ++it)
+            {
+                in >> *it;
+
+                if (in.fail())
+                {
+                    success = false;
+                    break;
+                }
+            }
+
+            if (success)
+            {
+                data = std::move(matrix);
+            }
+        }
+    }
+
+    // ensure an empty matrix is provided if reading has not been done successfully
+    if (!success)
+    {
+        data.clear();
+    }
+
+    return in;
+}
+
+template<typename T>
+std::ofstream& operator<<(std::ofstream& out, const Matrix<T>& data)
+{
+    for(int row{0}; row < data.getNrOfRows(); ++row)
+    {
+        for (typename Matrix<T>::ConstZIterator it{data.constZRowBegin(row)}; it != data.constZRowEnd(row); ++it)
+        {
+            out << *it << " ";
+        }
+
+        out << "\n";
+    }
+
+    return out;
+}
+
 
 #endif // MATRIXUTILS_H
