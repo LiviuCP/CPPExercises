3b06ed661c5cab1fe91313ff49f240a72aac39d4
Liviu C. Popa <liviucst@gmail.com>
2022-03-12 16:25:12 +0200

    Converted the binary search trees library to shared lib
    On Windows it was required to keep it static due to some issues encountered with dynamic libraries

---
+++ Algorithms/BST/BinarySearchTreesLib/binarysearchtreeslib.cpp
@@ -0,0 +1,5 @@
+#include "binarysearchtreeslib.h"
+
+BinarySearchTreesLib::BinarySearchTreesLib()
+{
+}
---
+++ Algorithms/BST/BinarySearchTreesLib/binarysearchtreeslib.h
@@ -0,0 +1,13 @@
+#ifndef BINARYSEARCHTREESLIB_H
+#define BINARYSEARCHTREESLIB_H
+
+#include "binarysearchtreeslib_global.h"
+
+class BINARYSEARCHTREESLIBSHARED_EXPORT BinarySearchTreesLib
+{
+
+public:
+    BinarySearchTreesLib();
+};
+
+#endif // BINARYSEARCHTREESLIB_H
