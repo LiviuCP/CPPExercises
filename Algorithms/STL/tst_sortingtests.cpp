f48a41cd1c3a6fed5f6c15160babf887f2793366
Liviu C. Popa <liviucst@gmail.com>
2022-09-17 21:38:59 +0300

    Added some unit tests for STL sorting algorithms
    This is the first part of a series of exercises meant for better understanding the usage of STL algorithms

---
+++ Algorithms/STL/tst_sortingtests.cpp
@@ -0,0 +1,145 @@
+#include <QtTest>
+
+#include <algorithm>
+#include <utility>
+#include <string>
+
+#include "matrix.h"
+
+using IntMatrix = Matrix<int>;
+using IntPair = std::pair<int, int>;
+using IntPairMatrix = Matrix<IntPair>;
+using StringIntPair = std::pair<std::string, int>;
+using StringIntPairMatrix = Matrix<StringIntPair>;
+
+class SortingTests : public QObject
+{
+    Q_OBJECT
+
+public:
+    SortingTests();
+
+private slots:
+    void testSorting();
+    void testStableSorting();
+    void testPartialSorting();
+    void testPartialSortingWithCopy();
+    void testNthElement();
+    void testIsSorted(); // is_sorted, is_sorted_until
+
+private:
+    IntMatrix mPrimaryIntMatrix;
+};
+
+SortingTests::SortingTests()
+    : mPrimaryIntMatrix{3, 4, {-1, 1, 1, 0, 2, 4, 5, 1, 3, -3, -3, 2}}
+{
+}
+
+void SortingTests::testSorting()
+{
+    StringIntPairMatrix matrix{2, 4, {{"Alex", 10}, {"Kevin", 11}, {"Alistair", 10}, {"George", 14}, {"Andrew", 11}, {"Cameron", 10}, {"Reggie", 12}, {"Patrick", 14}}};
+    const StringIntPairMatrix matrixRef{2, 4, {{"Alex", 10}, {"Alistair", 10}, {"George", 14}, {"Kevin", 11}, {"Cameron", 10}, {"Andrew", 11}, {"Reggie", 12}, {"Patrick", 14}}};
+
+    std::sort(matrix.zBegin(), matrix.zRowEnd(0));
+    std::sort(matrix.zRowBegin(1), matrix.zEnd(), [](StringIntPair firstPair, StringIntPair secondPair){return firstPair.second < secondPair.second;});
+
+    QVERIFY(matrix == matrixRef);
+}
+
+void SortingTests::testStableSorting()
+{
+    IntPairMatrix matrix{4, 3, {{1, 4}, {-1, 0}, {-1, 2}, {4, -5}, {1, 2}, {1, 3}, {-2, 4}, {2, 0}, {-3, -3}, {3, 2}, {-1, 0}, {3, -1}}};
+    IntPairMatrix matrixRef{4, 3, {{-3, -3}, {-2, 4}, {-1, 0}, {-1, 2}, {-1, 0}, {1, 4}, {1, 2}, {1, 3}, {2, 0}, {3, 2}, {3, -1}, {4, -5}}};
+
+    std::stable_sort(matrix.zBegin(), matrix.zEnd(), [](IntPair firstPair, IntPair secondPair){return firstPair.first < secondPair.first;});
+
+    QVERIFY(matrix == matrixRef);
+
+    matrix = {4, 3, {{4, 1}, {0, -1}, {2, -1}, {-5, 4}, {2, 1}, {3,1}, {4, -2}, {0, 2}, {-3, -3}, {2, 3}, {0, -1}, {-1, 3}}};
+    matrixRef = {4, 3, {{-3, -3}, {4, -2}, {0, -1}, {2, -1}, {0, -1}, {4, 1}, {2, 1}, {3, 1}, {0, 2}, {2, 3}, {-1, 3}, {-5, 4}}};
+
+    std::stable_sort(matrix.zBegin(), matrix.zEnd(), [](IntPair firstPair, IntPair secondPair){return firstPair.second < secondPair.second;});
+
+    QVERIFY(matrix == matrixRef);
+}
+
+void SortingTests::testPartialSorting()
+{
+    IntMatrix matrix{mPrimaryIntMatrix};
+    const IntMatrix matrixRef{3, 4, { -3, 0, 1, -9, -3, 1, -9, -9, -1, 1, -9, -9}};
+
+    std::partial_sort(matrix.nBegin(), matrix.getNIterator(1, 2), matrix.nEnd());
+
+    IntMatrix::ConstNIterator firstIt{matrix.constNBegin()};
+    IntMatrix::ConstNIterator secondIt{matrixRef.constNBegin()};
+
+    bool areElementsEqual{true};
+
+    while(firstIt != matrix.getConstNIterator(1, 2) && secondIt != matrixRef.getConstNIterator(1, 2))
+    {
+        areElementsEqual = areElementsEqual && (*firstIt == *secondIt);
+        ++firstIt;
+        ++secondIt;
+    }
+
+    QVERIFY(areElementsEqual);
+}
+
+void SortingTests::testPartialSortingWithCopy()
+{
+    IntMatrix srcMatrix{mPrimaryIntMatrix};
+    IntMatrix destMatrix{2, 2, {-2, 8, -5, 9}};
+    IntMatrix destMatrixRef{2, 2, {1, 5, 2, 9}};
+
+    std::partial_sort_copy(srcMatrix.constDBegin(1), srcMatrix.constDEnd(1), destMatrix.nBegin(), destMatrix.nEnd());
+
+    QVERIFY(destMatrix == destMatrixRef);
+
+    destMatrix = {2, 2, {-2, 8, -5, 9}};
+    destMatrixRef = {2, 2, {2, 8, -5, 1}};
+
+    std::partial_sort_copy(srcMatrix.constDBegin(1), srcMatrix.constDEnd(1), destMatrix.reverseDBegin(0), destMatrix.reverseDEnd(0));
+
+    QVERIFY(destMatrix == destMatrixRef);
+}
+
+void SortingTests::testNthElement()
+{
+    IntMatrix matrix{mPrimaryIntMatrix};
+    std::nth_element(matrix.nBegin(), matrix.getNIterator(1, 2), matrix.nEnd());
+
+    const IntMatrix::size_type nthElementRowNr{1};
+    const IntMatrix::size_type nthElementColumnNr{2};
+    IntMatrix::ConstNIterator nthElementIt{matrix.getConstNIterator(nthElementRowNr, nthElementColumnNr)};
+    const int expectedNthElementValue{2};
+
+    QVERIFY(expectedNthElementValue == *nthElementIt);
+
+    bool areLeftElementsLowerEqual{true};
+
+    for (IntMatrix::ConstNIterator it{matrix.constNBegin()}; it != matrix.getConstNIterator(nthElementRowNr, nthElementColumnNr); ++it)
+    {
+        areLeftElementsLowerEqual = areLeftElementsLowerEqual && (*it <= *nthElementIt);
+    }
+
+    bool areRightElementsHigherEqual{true};
+
+    for (IntMatrix::ConstNIterator it{nthElementIt + 1}; it != matrix.constNEnd(); ++it)
+    {
+        areRightElementsHigherEqual = areRightElementsHigherEqual && (*it >= *nthElementIt);
+    }
+
+    QVERIFY(areLeftElementsLowerEqual && areRightElementsHigherEqual);
+}
+
+void SortingTests::testIsSorted()
+{
+    QVERIFY(!std::is_sorted(mPrimaryIntMatrix.constZBegin(), mPrimaryIntMatrix.constZEnd()));
+    QVERIFY(std::is_sorted(mPrimaryIntMatrix.getConstZIterator(0, 3), mPrimaryIntMatrix.getConstZIterator(1, 2)));
+    QVERIFY(std::is_sorted_until(mPrimaryIntMatrix.constZBegin(), mPrimaryIntMatrix.constZEnd()) == mPrimaryIntMatrix.getConstZIterator(0, 3));
+}
+
+QTEST_APPLESS_MAIN(SortingTests)
+
+#include "tst_sortingtests.moc"
