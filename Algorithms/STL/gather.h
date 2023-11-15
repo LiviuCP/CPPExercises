119d7bf13763f2bcdba1696a65e1d05bed3de992
Liviu C. Popa <liviucst@gmail.com>
2023-11-15 23:23:40 +0200

    Refactored the bidimensional gathering algorithm
    It was required to use more suggestive variables in order to improve the self-explanatory trait

--- Algorithms/STL/gather.h
+++ Algorithms/STL/gather.h
@@ -3,6 +3,11 @@
 
 #include "datautils.h"
 
+/* Unidimensional gathering:
+   - elements from a sequence are gathered around a chosen position (gathering point) by using a unary predicate
+   - generic implementation using bidirectional iterators
+*/
+
 template<typename BidirectionalIt, typename Predicate>
 std::pair<BidirectionalIt, BidirectionalIt> gatherSequenceElements(BidirectionalIt startIt, BidirectionalIt endIt, BidirectionalIt gatheringPointIt, Predicate predicate)
 {
@@ -20,6 +25,12 @@ std::pair<BidirectionalIt, BidirectionalIt> gatherSequenceElements(Bidirectional
     return {gatheringStartIt, gatheringEndIt};
 }
 
+/* Bidimensional gathering:
+   - elements from a Matrix gathered around a chosen point by using a unary predicate
+   - "specialized" implementation, so called Matrix "points" used instead of iterators; unidimensional gather also used as building block
+   - gathering performed row-by-row by using the y coordinate of the gathering point; the result is further gathered column-by-column by using the x coordinate of the gathering point
+*/
+
 template<typename DataType>
 std::tuple<MatrixPoint<DataType>,
            MatrixPoint<DataType>,
@@ -29,69 +40,72 @@ std::tuple<MatrixPoint<DataType>,
                       MatrixPoint<DataType> gatheringPoint,
                       std::function<bool(const DataType&)>& predicate)
 {
-    MatrixPoint<DataType> gatheringStartingPoint{0, 0};
-    MatrixPoint<DataType> gatheringEndingPoint{0, 0};
+    MatrixPoint<DataType> innerRectStartingPoint{0, 0};
+    MatrixPoint<DataType> innerRectEndingPoint{0, 0};
     MatrixSizeType<DataType> gatheredElementsCount{0};
 
     if (!matrix.isEmpty())
     {
+        // normalize the starting/ending points to the matrix dimensions in order to get valid input for determining the outer bounding rectangle
         startingPoint.first = std::clamp(startingPoint.first, 0, matrix.getNrOfRows());
         startingPoint.second = std::clamp(startingPoint.second, 0, matrix.getNrOfColumns());
         endingPoint.first = std::clamp(endingPoint.first, 0, matrix.getNrOfRows());
         endingPoint.second = std::clamp(endingPoint.second, 0, matrix.getNrOfColumns());
 
-        // get the input bounding rectangle
-        MatrixSizeType<DataType> const c_StartingRowNr{std::min(startingPoint.first, endingPoint.first)};
-        MatrixSizeType<DataType> const c_EndingRowNr{std::max(startingPoint.first, endingPoint.first)};
-        MatrixSizeType<DataType> const c_StartingColumnNr{std::min(startingPoint.second, endingPoint.second)};
-        MatrixSizeType<DataType> const c_EndingColumnNr{std::max(startingPoint.second, endingPoint.second)};
+        // get the outer bounding rectangle from which elements are being gathered (based on starting/ending points)
+        MatrixSizeType<DataType> const c_OuterRectStartingRowNr{std::min(startingPoint.first, endingPoint.first)};
+        MatrixSizeType<DataType> const c_OuterRectEndingRowNr{std::max(startingPoint.first, endingPoint.first)};
+        MatrixSizeType<DataType> const c_OuterRectStartingColumnNr{std::min(startingPoint.second, endingPoint.second)};
+        MatrixSizeType<DataType> const c_OuterRectEndingColumnNr{std::max(startingPoint.second, endingPoint.second)};
 
-        gatheringPoint.first = std::clamp(gatheringPoint.first, c_StartingRowNr, c_EndingRowNr);
-        gatheringPoint.second = std::clamp(gatheringPoint.second, c_StartingColumnNr, c_EndingColumnNr);
+        // gathering point should belong to the outer bounding rectangle (and finally to the resulting inner rectangle)
+        gatheringPoint.first = std::clamp(gatheringPoint.first, c_OuterRectStartingRowNr, c_OuterRectEndingRowNr);
+        gatheringPoint.second = std::clamp(gatheringPoint.second, c_OuterRectStartingColumnNr, c_OuterRectEndingColumnNr);
 
-        // start from the opposite end of each interval in order to determine the min/max row/column numbers that limit the gathered elements
-        MatrixSizeType<DataType> minGatheringRowNr{c_EndingRowNr};
-        MatrixSizeType<DataType> maxGatheringRowNr{c_StartingRowNr};
-        MatrixSizeType<DataType> minGatheringColumnNr{c_EndingColumnNr};
-        MatrixSizeType<DataType> maxGatheringColumnNr{c_StartingColumnNr};
+        // start from the ending (maximum) outer rectangle parameter to get the corresponding starting (minimum) inner rectangle parameter (and vice-versa)
+        MatrixSizeType<DataType> innerRectStartingRowNr{c_OuterRectEndingRowNr};
+        MatrixSizeType<DataType> innerRectEndingRowNr{c_OuterRectStartingRowNr};
+        MatrixSizeType<DataType> innerRectStartingColumnNr{c_OuterRectEndingColumnNr};
+        MatrixSizeType<DataType> innerRectEndingColumnNr{c_OuterRectStartingColumnNr};
 
-        for(auto rowNr{c_StartingRowNr}; rowNr < c_EndingRowNr; ++rowNr)
+        for(MatrixSizeType<DataType> rowNr{c_OuterRectStartingRowNr}; rowNr < c_OuterRectEndingRowNr; ++rowNr)
         {
-            auto rowRangeStartIt{c_StartingColumnNr == matrix.getNrOfColumns() ? matrix.zRowEnd(rowNr) : matrix.getZIterator(rowNr, c_StartingColumnNr)};
-            auto rowRangeEndIt{c_EndingColumnNr == matrix.getNrOfColumns() ? matrix.zRowEnd(rowNr) : matrix.getZIterator(rowNr, c_EndingColumnNr)};
+            auto rowGatheringRangeStartIt{c_OuterRectStartingColumnNr == matrix.getNrOfColumns() ? matrix.zRowEnd(rowNr) : matrix.getZIterator(rowNr, c_OuterRectStartingColumnNr)};
+            auto rowGatheringRangeEndIt{c_OuterRectEndingColumnNr == matrix.getNrOfColumns() ? matrix.zRowEnd(rowNr) : matrix.getZIterator(rowNr, c_OuterRectEndingColumnNr)};
             auto rowGatheringPointIt{gatheringPoint.second == matrix.getNrOfColumns() ? matrix.zRowEnd(rowNr) : matrix.getZIterator(rowNr, gatheringPoint.second)};
 
-            const auto&[gatheringRowStartIt, gatheringRowEndIt]{gatherSequenceElements(rowRangeStartIt, rowRangeEndIt, rowGatheringPointIt, predicate)};
-            minGatheringColumnNr = std::min(minGatheringColumnNr, gatheringRowStartIt == matrix.zRowEnd(rowNr) ? matrix.getNrOfColumns() : gatheringRowStartIt.getColumnNr());
-            maxGatheringColumnNr = std::max(maxGatheringColumnNr, gatheringRowEndIt == matrix.zRowEnd(rowNr) ? matrix.getNrOfColumns() : gatheringRowEndIt.getColumnNr());
-            gatheredElementsCount += std::distance(gatheringRowStartIt, gatheringRowEndIt);
+            const auto&[rowGatheredRangeStartIt, rowGatheredRangeEndIt]{gatherSequenceElements(rowGatheringRangeStartIt, rowGatheringRangeEndIt, rowGatheringPointIt, predicate)};
+            innerRectStartingColumnNr = std::min(innerRectStartingColumnNr, rowGatheredRangeStartIt == matrix.zRowEnd(rowNr) ? matrix.getNrOfColumns() : rowGatheredRangeStartIt.getColumnNr());
+            innerRectEndingColumnNr = std::max(innerRectEndingColumnNr, rowGatheredRangeEndIt == matrix.zRowEnd(rowNr) ? matrix.getNrOfColumns() : rowGatheredRangeEndIt.getColumnNr());
+            gatheredElementsCount += std::distance(rowGatheredRangeStartIt, rowGatheredRangeEndIt); // elements are counted only once (during per-row gathering)
         }
 
-        for(auto columnNr{c_StartingColumnNr}; columnNr < c_EndingColumnNr; ++columnNr)
+        for(MatrixSizeType<DataType> columnNr{c_OuterRectStartingColumnNr}; columnNr < c_OuterRectEndingColumnNr; ++columnNr)
         {
-            auto columnRangeStartIt{c_StartingRowNr == matrix.getNrOfRows() ? matrix.nColumnEnd(columnNr) : matrix.getNIterator(c_StartingRowNr, columnNr)};
-            auto columnRangeEndIt{c_EndingRowNr == matrix.getNrOfRows() ? matrix.nColumnEnd(columnNr) : matrix.getNIterator(c_EndingRowNr, columnNr)};
+            auto columnGatheringRangeStartIt{c_OuterRectStartingRowNr == matrix.getNrOfRows() ? matrix.nColumnEnd(columnNr) : matrix.getNIterator(c_OuterRectStartingRowNr, columnNr)};
+            auto columnGatheringRangeEndIt{c_OuterRectEndingRowNr == matrix.getNrOfRows() ? matrix.nColumnEnd(columnNr) : matrix.getNIterator(c_OuterRectEndingRowNr, columnNr)};
             auto columnGatheringPointIt{gatheringPoint.first == matrix.getNrOfRows() ? matrix.nColumnEnd(columnNr) : matrix.getNIterator(gatheringPoint.first, columnNr)};
 
-            const auto&[gatheringColumnStartIt, gatheringColumnEndIt]{gatherSequenceElements(columnRangeStartIt, columnRangeEndIt, columnGatheringPointIt, predicate)};
-            minGatheringRowNr = std::min(minGatheringRowNr, gatheringColumnStartIt == matrix.nColumnEnd(columnNr) ? matrix.getNrOfRows() : gatheringColumnStartIt.getRowNr());
-            maxGatheringRowNr = std::max(maxGatheringRowNr, gatheringColumnEndIt == matrix.nColumnEnd(columnNr) ? matrix.getNrOfRows() : gatheringColumnEndIt.getRowNr());
+            const auto&[columnGatheredRangeStartIt, columnGatheredRangeEndIt]{gatherSequenceElements(columnGatheringRangeStartIt, columnGatheringRangeEndIt, columnGatheringPointIt, predicate)};
+            innerRectStartingRowNr = std::min(innerRectStartingRowNr, columnGatheredRangeStartIt == matrix.nColumnEnd(columnNr) ? matrix.getNrOfRows() : columnGatheredRangeStartIt.getRowNr());
+            innerRectEndingRowNr = std::max(innerRectEndingRowNr, columnGatheredRangeEndIt == matrix.nColumnEnd(columnNr) ? matrix.getNrOfRows() : columnGatheredRangeEndIt.getRowNr());
         }
 
-        // determine the output bounding rectangle (should be the gathering point if the input bounding rectangle is a row, column or single element)
-        if (minGatheringRowNr != maxGatheringRowNr && minGatheringColumnNr != maxGatheringColumnNr)
+        // determine the inner bounding rectangle (minimum bounding rectangle containing all gathered elements) - contained within or at most equal to the outer rectangle
+        if (innerRectStartingRowNr != innerRectEndingRowNr && innerRectStartingColumnNr != innerRectEndingColumnNr)
         {
-            gatheringStartingPoint = {minGatheringRowNr, minGatheringColumnNr};
-            gatheringEndingPoint = {maxGatheringRowNr, maxGatheringColumnNr};
+            innerRectStartingPoint = {innerRectStartingRowNr, innerRectStartingColumnNr};
+            innerRectEndingPoint = {innerRectEndingRowNr, innerRectEndingColumnNr};
         }
         else
         {
-            gatheringStartingPoint = gatheringPoint;
-            gatheringEndingPoint = gatheringPoint;
+            // the inner rectangle decays to the gathering point if the starting and ending point are on the same row/column
+            innerRectStartingPoint = gatheringPoint;
+            innerRectEndingPoint = gatheringPoint;
         }
     }
 
-    return {gatheringStartingPoint, gatheringEndingPoint, gatheredElementsCount};
+    return {innerRectStartingPoint, innerRectEndingPoint, gatheredElementsCount};
 }
 
 #endif // GATHER_H
